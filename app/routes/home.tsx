1040ebdda622b4089d1d3fa8c78c95d87573e984
ASHUTOSH-KUMAR-RAO <icoashutosh@gmail.com>
2025-08-08 11:55:33 +0530

    01:home page

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,32 @@
+import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { resumes } from "../../constants";
+import ResumeCard from "~/components/ResumeCard";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Resumind" },
+    { name: "description", content: "Smart Feedback for your dream job" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+      <Navbar />
+      <section className="main-section">
+        <div className="page-heading">
+          <h1>Monitor Your Job Application Success</h1>
+          <h2>✨ Review Submission & Receive AI-Powered Insights</h2>
+        </div>
+      {resumes.length > 0 && (
+        <div className="resumes-section">
+          {resumes.map((resume) => (
+            <ResumeCard key={resume.id} resume={resume} />
+          ))}
+        </div>
+      )}
+      </section>
+    </main>
+  );
 }

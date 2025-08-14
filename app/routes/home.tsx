15b90e6c2bedc8cb4f8fdea2f2868f73d3448457
ASHUTOSH-KUMAR-RAO <icoashutosh@gmail.com>
2025-08-14 17:43:10 +0530

    07:fetching-real-resume

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,10 +1,9 @@
 import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
-import { resumes } from "../../constants";
 import ResumeCard from "~/components/ResumeCard";
 import { usePuterStore } from "~/lib/puter";
-import { useNavigate } from "react-router";
-import { useEffect } from "react";
+import { Link, useNavigate } from "react-router";
+import { useEffect, useState } from "react";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -15,14 +14,37 @@ export function meta({}: Route.MetaArgs) {
 
 export default function Home() {
   // ✅ Correct way - hooks directly in component
-  const { auth } = usePuterStore();
+  const { auth, kv } = usePuterStore();
   const navigate = useNavigate();
-  
+
+  const [resumes, setResumes] = useState<Resume[]>([]);
+  const [loadingResumes, setLoadingResumes] = useState(false);
+
   useEffect(() => {
     if (!auth.isAuthenticated) {
       navigate("/auth?next=/");
     }
   }, [auth.isAuthenticated, navigate]);
+
+  useEffect(() => {
+    const loadResumes = async () => {
+      setLoadingResumes(true);
+
+      const resumes = (await kv.list("resume:*", true)) as KVItem[];
+
+      const parshedResumes = resumes?.map(
+        (resume) => JSON.parse(resume.value) as Resume
+      );
+
+      console.log("parshedResumes :", parshedResumes);
+
+      setResumes(parshedResumes || []);
+
+      setLoadingResumes(false);
+    };
+
+    loadResumes();
+  }, []);
 
   return (
     <main className="bg-[url('/images/bg-main.svg')] bg-cover">
@@ -30,13 +52,37 @@ export default function Home() {
       <section className="main-section">
         <div className="page-heading">
           <h1>Monitor Your Job Application Success</h1>
-          <h2>✨ Review Submission & Receive AI-Powered Insights</h2>
+          {!loadingResumes && resumes.length === 0 ? (
+            <h2>
+              No Resume Fount.Upload Your Resume First,then get your feedback
+            </h2>
+          ) : (
+            <h2>✨ Review Submission & Receive AI-Powered Insights</h2>
+          )}
+
+          {loadingResumes && (
+            <div className="flex flex-col items-center justify-center">
+              <img
+                src="/images/resume-scan-2.gif"
+                alt="loader"
+                className="w-[200px]"
+              />
+            </div>
+          )}
         </div>
-        {resumes.length > 0 && (
+        {!loadingResumes && resumes.length > 0 && (
           <div className="resumes-section">
             {resumes.map((resume) => (
               <ResumeCard key={resume.id} resume={resume} />
             ))}
+          </div>
+        )}
+
+        {!loadingResumes && (
+          <div className="flex flex-col items-center justify-center mt-10 gap-4">
+            <Link to="/Upload" className="primary-button w-fit text-xl font-semibold">
+              Upload Resume
+            </Link>
           </div>
         )}
       </section>
